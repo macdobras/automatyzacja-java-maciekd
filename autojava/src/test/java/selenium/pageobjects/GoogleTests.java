a01bc1110720765a8b6b41809fdfe211e101a79d
macdobras <maciek.dobrasiak@op.pl>
2018-12-12 17:57:00 +0100

    aktualizacja GoogleTests - POP

--- autojava/src/test/java/selenium/pageobjects/GoogleTests.java
+++ autojava/src/test/java/selenium/pageobjects/GoogleTests.java
@@ -1,20 +1,27 @@
 package selenium.pageobjects;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class GoogleTests extends BaseTest{
 
     @Test
     public void FindCodeSprinters(){
+
         //open Google Page
         driver.get("https://www.google.com/");
 
+
         //search for Code Sprinters Page
-        driver.findElement(By.name("q"));
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("Code Sprinters");
+        searchBox.submit();
 
 
         //assert Code Sprinters Page is found
+        Assertions.assertTrue();
 
 
     }
